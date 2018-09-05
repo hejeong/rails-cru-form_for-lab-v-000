0d0e7ccaca6282fb909eeab87ec481c699ce75d8
Jonathan Hong <jonhong777@gmail.com>
2018-09-05 13:24:27 +0000

    automatically backed up by learn

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,30 @@
+class SongsController < ApplicationController
+  def new
+
+  end
+
+  def create
+    @song = Song.create(song_params(:name, :artist_id, :genre_id))
+    redirect_to song_path(@song)
+  end
+
+  def show
+    @song = Song.find_by_id(:id)
+  end
+
+  def edit
+    @song = Song.find_by_id(:id)
+  end
+
+  def update
+    @song = Song.find_by_id(:id)
+    @song.update(song_params(:name, :author_id, :genre_id))
+    redirect_to song_path(@song)
+  end
+
+  private
+
+  def song_params(*args)
+    params.require(:song).permit(*args)
+  end
+end
